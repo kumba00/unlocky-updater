5da857814b92ca1d4cb1918e0d2e0b4ad6e22182
kumba00 <145571860+kumba00@users.noreply.github.com>
2026-09-09 09:38:28 +0100

    Update version1.php

--- version1.php
+++ version1.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-
-
-
 <?php
 echo "1.0.8";
 ?>
-
-
-<script>
-    
-</script>
-
-</body>
-</html>
